f37adca42b40cfdac755004f38c87de66ced157d
Frenzgyn <101758365+Frenzgyn@users.noreply.github.com>
2022-06-12 18:23:37 +0200

    Update fatesp.d
    Commented out a portion to avoid ending up with a duplicate D#Silverstar, not leveled up.

--- star/dialogues/fatesp.d
+++ star/dialogues/fatesp.d
@@ -4,8 +4,8 @@ EXTEND_BOTTOM FATESP 6
 Global("SilverFMT","GLOBAL",0)
 !InMyArea("D#Silver")
 Global("D#SilverSummoned","GLOBAL",0)~ THEN REPLY @1
- DO ~CreateVisualEffect("SPPORTAL",[1999.1228])
-Wait(2)
-CreateCreature("D#Silver",[1999.1228],0)
+// DO ~CreateVisualEffect("SPPORTAL",[1999.1228])
+//Wait(2)
+//CreateCreature("D#Silver",[1999.1228],0)
 SetGlobal("D#SilverSummoned","GLOBAL",1)~ GOTO 8
 END
